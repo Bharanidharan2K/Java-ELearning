6f65e8de6c208a01f52ed25e4bbbc1e2451fd2a3
BharanidharanKathiresan <bharanikkab123@gmail.com>
2021-05-31 20:53:42 +0530

    Conditional-salary_computation

---
+++ Conditional-Prog/com/salary_computation/Main.java
@@ -0,0 +1,24 @@
+package com.salary_computation;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float HRA,DA;
+		int salary = sc.nextInt();
+		if(salary < 15000) {
+			HRA = salary*0.15F;
+			DA = salary*0.9F;
+			System.out.printf("%.2f",(salary+HRA+DA));
+		}
+		else {
+			HRA = 5000;
+			DA = salary*0.98F;
+			System.out.printf("%.2f",(salary+HRA+DA));
+		}
+
+	}
+
+}
