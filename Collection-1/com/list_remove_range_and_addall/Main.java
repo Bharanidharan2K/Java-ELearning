caf3d583df54a75d47afba51f87ca29b23e43a92
K-Bharanidharan <bharanikkab123@gmail.com>
2021-06-14 16:23:38 +0530

    Collection-1-list_remove_range_and_addall

---
+++ Collection-1/com/list_remove_range_and_addall/Main.java
@@ -0,0 +1,32 @@
+package com.list_remove_range_and_addall;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		UserBO userBo = new UserBO();
+		UserBO userList = UserBO.getList();
+		System.out.println("Enter the number of User details to be added");
+		int n = Integer.parseInt(br.readLine());
+		for(int i=1;i<=n;i++) {
+			System.out.println("Enter the user "+i+" detail in csv format");
+			String things[] = br.readLine().split(",");
+			User user = new User(things[0], things[1], things[2], things[3]);
+			userBo.add(user);
+			
+		}
+		userList.addAll(userBo);
+		User u = new User();
+		u.display(userList);
+		System.out.println("Enter the range to be removed from array list");
+		int startPoint = Integer.parseInt(br.readLine());
+		int endPoint = Integer.parseInt(br.readLine());
+		userList.removeUser(startPoint, endPoint);
+		u.display(userList);
+		br.close();
+	}
+
+}
---
+++ Collection-1/com/list_remove_range_and_addall/User.java
@@ -0,0 +1,51 @@
+package com.list_remove_range_and_addall;
+
+import java.util.ArrayList;
+
+public class User {
+	private String name;	
+	private String contactNumber;	
+	private String userName;	
+	private String email;
+	public User() {
+		super();
+	}
+	public User(String name, String contactNumber, String userName, String email) {
+		super();
+		this.name = name;
+		this.contactNumber = contactNumber;
+		this.userName = userName;
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void display(ArrayList<User> list) {
+		System.out.printf("%-20s%-20s%-20s%-20s\n","Name", "Contact Number", "Username", "Email");
+		for(User i : list) {
+			System.out.printf("%-20s%-20s%-20s%-20s\n",
+					i.getName(),i.getContactNumber(), i.getUserName(), i.getEmail());
+		}
+	}
+}
