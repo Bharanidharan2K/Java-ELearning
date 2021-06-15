5436615de70945ad7e933cc21437bf69d010b0e7
K-Bharanidharan <bharanikkab123@gmail.com>
2021-06-15 14:18:28 +0530

    Collection-1-user_search

---
+++ Collection-1/com/user_search/Main.java
@@ -0,0 +1,50 @@
+package com.user_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Set<User> set = new HashSet<>();
+		System.out.println("Enter number of users:");
+		Integer n = Integer.parseInt(br.readLine());
+		for(int i = 0,index = 1;i<n;i++) {
+			System.out.println("Enter details of user"+index++);
+			System.out.println("Username:");
+			String username = br.readLine();
+			System.out.println("Bank name:");
+			String bankname = br.readLine();
+			User user = new User(username, bankname);
+			set.add(user);
+		}
+		ArrayList<User> list1 = new ArrayList<>(set);
+		ArrayList<User> list2 = new ArrayList<>();
+		Collections.sort(list1);
+		System.out.println("Enter username(Expire in one month) seperated by comma");
+		String things[] = br.readLine().split(",");
+		System.out.println("Users going to expire within a month");
+		for(String i : things) {
+			User u = new User(i, null);
+			list2.add(u);
+		}
+		int index = 1;
+		for(User i : list1) {
+			for(User j : list2) {
+				if(i.equals(j)) {
+					System.out.println("User "+index++);
+					System.out.println("User Name ="+i.getUsername());
+					System.out.println("Bank Name ="+i.getBankname());
+				}
+			}
+		}
+		br.close();
+
+	}
+
+}
---
+++ Collection-1/com/user_search/User.java
@@ -0,0 +1,34 @@
+package com.user_search;
+
+public class User implements Comparable<User>{
+	private String username;	
+	private String bankname;
+	
+	public User() {}
+	public User(String username, String bankname) {
+		this.username = username;
+		this.bankname = bankname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getBankname() {
+		return bankname;
+	}
+	public void setBankname(String bankname) {
+		this.bankname = bankname;
+	}
+	@Override
+	public int compareTo(User o) {
+		return this.getUsername().compareTo(o.getUsername());
+	}
+	public boolean equals(User o) {
+		return this.getUsername().equals(o.getUsername());
+	}
+	
+	
+	
+}
