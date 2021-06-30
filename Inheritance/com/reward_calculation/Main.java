705532a72a619710e79fbdad43c5c8225ab4576c
K-Bharanidharan <bharanikkab123@gmail.com>
2021-06-30 15:02:57 +0530

    Inheritance-reward_calculation

--- Inheritance/com/reward_calculation/Main.java
+++ Inheritance/com/reward_calculation/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		String check = "";
 		do {
 			System.out.println("Enter the holder name");
